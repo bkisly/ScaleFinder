0e21b97db7b1ebc1b0e1ea2d2342cd837d8597f0
bkisly <b.kisly@gmail.com>
2022-05-01 02:37:30 +0200

    Created ToggleButton control

--- ScaleFinder/ScaleFinder/App.xaml.cs
+++ ScaleFinder/ScaleFinder/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ScaleFinder.Views;
 
 namespace ScaleFinder
 {
@@ -10,7 +11,7 @@ namespace ScaleFinder
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new FinderPage();
         }
 
         protected override void OnStart()
---
+++ ScaleFinder/ScaleFinder/Views/ToggleButton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ScaleFinder.Views
+{
+    internal class ToggleButton : Button
+    {
+        public event EventHandler<ToggledEventArgs> Toggled;
+
+        public static BindableProperty IsToggledProperty =
+            BindableProperty.Create("IsToggled", typeof(bool), typeof(ToggleButton), false,
+                                    propertyChanged: OnIsToggledChanged);
+        public bool IsToggled
+        {
+            get { return (bool)GetValue(IsToggledProperty); }
+            set { SetValue(IsToggledProperty, value); }
+        }
+
+        public ToggleButton()
+        {
+            Clicked += (sender, args) => IsToggled ^= true;
+        }
+
+        protected override void OnParentSet()
+        {
+            base.OnParentSet();
+            VisualStateManager.GoToState(this, "ToggledOff");
+        }
+
+        static void OnIsToggledChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            ToggleButton toggleButton = (ToggleButton)bindable;
+            bool isToggled = (bool)newValue;
+
+            // Fire event
+            toggleButton.Toggled?.Invoke(toggleButton, new ToggledEventArgs(isToggled));
+
+            // Set the visual state
+            VisualStateManager.GoToState(toggleButton, isToggled ? "ToggledOn" : "ToggledOff");
+        }
+    }
+}
